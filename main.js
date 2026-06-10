c27f7feeca9ea6ef56f1ed6e49d88c24dd553b73
RafaelKoti <kotynnn5@gmail.com>
2026-06-10 19:42:57 -0300

    cRIando a função p/ a verificação se a quantidade é = a 0

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 // Arquivo para código javascript
 
 const API_URL = 'https://6a1f56a9b79eec0d6cf0a932.mockapi.io/api/v1/users';
+
+function validarRetirada(estoque, quantidade) {
+  if (quantidade <= 0) return false;
+  if (quantidade > estoque) return false;
+  return true;
+}
