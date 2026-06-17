14d78fe3baf8826dcf56d2de4b28ff05e518c85c
RafaelKoti <kotynnn5@gmail.com>
2026-06-17 19:19:01 -0300

    Criando a função p/ ver se a retirada pode ser feita

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 var API_URL = 'https://6a1f56a9b79eec0d6cf0a932.mockapi.io/api/v1/users';
 
+function validarRetirada (estoqueAtual, quantidadeRetirada){
+  if (quantidadeRetirada <= 0) return false;
+  if (quantidadeRetirada > estoqueAtual) return false;
+  return true;
+}
+
 function carregarMateriais() {
   fetch(API_URL)
     .then(function(res) { return res.json(); })
